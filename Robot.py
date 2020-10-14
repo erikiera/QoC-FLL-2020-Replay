d635c8374d58b2b84d094d5fe2eda8a7785ecdb9
Erik Anderson <erik@erik-anderson.net>
2020-10-14 07:53:24 -0400

    2020-10-13

--- Robot.py
+++ Robot.py
@@ -7,17 +7,21 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from math import copysign, sin
+from pybricks.iodevices import Ev3devSensor
+from os import path
+
 
 class LightSensor(ColorSensor):
-    low=0
-    high=120
     black = 20
     white = 80
+    line = 50
 
-    def __init__(self):
-        black = self.low + (self.high-self.low)*.2
-        white = self.low + (self.high-self.low)*.8
-        
+    def __init__(self, port, low = 10, high = 120):
+        super().__init__(port)
+        self.black = low + (high-low)*.2
+        self.white = low + (high-low)*.8
+        self.line = low + (high-low)*.5
+
     def light(self):
         return sum(self.rgb())
 
@@ -44,19 +48,56 @@ class LightSensor(ColorSensor):
     def waitForLine(self):
         self.waitForWhite()
         self.waitForBlack()
-
-
+    
 class Robot():
     def __init__(self):
         self.frontMotor=Motor(Port.D)
         self.rearMotor=Motor(Port.A)
         self.leftMotor=Motor(Port.C)
         self.rightMotor=Motor(Port.B)
-        self.leftSensor=ColorSensor(Port.S2)
-        self.rightSensor=ColorSensor(Port.S3)
+
+        if path.exists('calibrate.py'):
+            import calibrate
+            self.leftSensor=LightSensor(Port.S3, calibrate.leftLow, calibrate.leftHigh)
+            self.rightSensor=LightSensor(Port.S2, calibrate.rightLow, calibrate.rightHigh)
+
+        else: 
+            self.leftSensor=LightSensor(Port.S3, 10, 105)
+            self.rightSensor=LightSensor(Port.S2, 20, 160)
+
         self.gyroSensor=GyroSensor(Port.S1)
         self.gyroSensor.reset_angle(0)
+        self.testSensor = Ev3devSensor(Port.S3)
 
+    def calibrate(self):
+        rightHigh = 40
+        rightLow = 70
+        leftHigh = 40
+        leftLow = 70
+
+        timer = StopWatch()
+        self.moveSteering(0, 125)
+        while timer.time() < 5000:
+            if self.rightSensor.light() > rightHigh:
+                rightHigh = self.rightSensor.light()
+            if self.rightSensor.light() < rightLow:
+                rightLow = self.rightSensor.light()
+            if self.leftSensor.light() > leftHigh:
+                leftHigh = self.leftSensor.light()
+            if self.leftSensor.light() < leftLow:
+                leftLow = self.leftSensor.light()
+        self.stop()
+        # write results to file
+        with open('calibrate.py', 'w') as myFile:
+            myFile.write('leftLow = ')
+            myFile.write(str(leftLow))
+            myFile.write('\nrightLow = ', rightLow)
+            myFile.write(str(rightLow))
+            myFile.write('\nleftHigh = ', leftHigh)
+            myFile.write(str(leftHigh))
+            myFile.write('\nrightHigh = ', rightHigh)
+            myFile.write(str(rightHigh))
+        
     def moveSteering(self, steering, speed):
         leftMotorSpeed = speed * min(1, 0.02 * steering + 1)
         rightMotorSpeed = speed * min (1, -0.02 * steering + 1)
@@ -68,13 +109,11 @@ class Robot():
         kSteering=5     # Steering coefficient
         startDegrees=self.gyroSensor.angle()
         # Startup for ramp speed
-        rotation= distance*2*3.14*6.28
-        self.rightMotor.reset_angle(0)
-
         if distance < 0 :
             distance = abs(distance)
             speed = -speed
-
+        rotation= distance*2*3.14*6.28
+        self.rightMotor.reset_angle(0)
         #  Loop
         while abs(self.rightMotor.angle()) < abs(rotation)  :
             # Do the gyro steering stuff
@@ -82,8 +121,7 @@ class Robot():
             errorGyro=currentDegrees-startDegrees
             # Do the ramp speed stuff   
             rampSpeed=sin(abs(self.rightMotor.angle()) / rotation * 3.14)
-            self.moveSteering(errorGyro*kSteering, rampSpeed * speed + copysign(20, speed))
-
+            self.moveSteering(errorGyro*kSteering*copysign(1, speed), rampSpeed * speed + copysign(20, speed))
         # Exit
         self.stop()
 
@@ -105,27 +143,46 @@ class Robot():
         self.stop()
 
     def lineFollow2Line(self, speed, rightSide=True, rightStop=True):
-        pass
+        # Loop
+        while not self.leftSensor.isWhite():
+            error = self.rightSensor.line - self.rightSensor.light()
+            correction = error * 0.3
+            self.moveSteering(correction, speed)
+        self.stop()
 
     def lineFollow4Time(self, speed, time, rightSide=True):
         pass
 
     def turn2Line(self, speed, time=5):
-        pass
+        self.moveSteering(100,speed)
+        self.rightSensor.waitForLine()
+        self.stop()
     
     def drive2Line(self, speed, distanceBefore, distanceAfter):
         # Startup
         self.drive(distanceBefore, speed)
 
         # Loop
-        while self.rightSensor.reflection() < 90:
-            self.moveSteering(0, 70)
+        #while self.rightSensor.rgb() < 90:
+        #    self.moveSteering(0, 70)
+        # Start Driving
+        self.moveSteering(0, 70)
+        # execute function wait until we detect a line
+        self.rightSensor.waitForLine()
+        self.stop()
 
         # Exit
         self.drive(distanceAfter, speed)
 
-    
     def stop(self, brake=Stop.BRAKE):
         # 3 options: Stop.BRAKE, Stop.COAST, Stop.HOLD
         self.rightMotor.stop(brake)
         self.leftMotor.stop(brake)
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,5 +2,37 @@
 from Robot import *
 
 robot=Robot()
-robot.drive(35, 500)
-robot.drive(10, 300)
+
+if False:
+    robot.drive(35, 500)
+    robot.moveSteering(0, 20)
+    wait (20000)
+    robot.stop()
+    robot.drive(8, -400)
+    robot.turn(-90, 300)
+    robot.drive(-50, 450)
+
+robot.lineFollow2Line(200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#robot.drive(35, 500)
+#robot.drive(10, 300)
+
+
+#robot.drive2Line(200, 2, 0)
